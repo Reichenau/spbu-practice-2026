98417aa651a28d1350aa122e477129d140467592
pavlovmisha <90309912+pavlovmisha@users.noreply.github.com>
2026-06-29 15:22:09 +0300

    Delete dsl directory

--- dsl/actors/execution/__init__.py
+++
@@ -1,9 +0,0 @@
-from .execution_coordinator_actor import DslExecutionCoordinatorActor
-from .execution_result_collector_actor import DslExecutionResultCollectorActor
-from .execution_worker_actor import DslExecutionWorkerActor
-
-__all__ = [
-    "DslExecutionCoordinatorActor",
-    "DslExecutionResultCollectorActor",
-    "DslExecutionWorkerActor",
-]
